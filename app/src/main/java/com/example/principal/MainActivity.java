1704763feb09096b76b3abd3e69c0687be59f20e
Ander A <Ander@POB-DE-3568-24.cebancdea.edu>
2020-01-07 09:01:44 +0100

    Initial commit

---
+++ app/src/main/java/com/example/principal/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.principal;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Obtenemos el mapa de forma asíncrona (notificará cuando esté listo)
+        SupportMapFragment mapFragment = (SupportMapFragment)
+                getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        GoogleMap mapa = googleMap;
+        LatLng UPV = new LatLng(39.481106, -0.340987); //Nos ubicamos en la UPV
+        mapa.addMarker(new MarkerOptions().position(UPV).title("Marker UPV"));
+        mapa.moveCamera(CameraUpdateFactory.newLatLng(UPV));
+    }
+}
+
+
